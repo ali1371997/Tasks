c82ce924f66305c865dd800a49946614b8cbc4ff
Ali Amin <aliaminzewil@gmail.com>
2020-02-27 20:28:27 +0200

    first

--- Timer_Task/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"F:/ARM/tiva-c-master/tiva-c-master/driverlib/ccs/Debug/driverlib.lib"
-
